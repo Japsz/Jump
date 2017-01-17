76e4b73342e71d4a1fc0ecd0a1c14e0c23e88e9c
Japsz <benjamin.meneses.14@sansano.usm.cl>
2017-01-17 13:00:40 -0300

    Pulseras

--- routes/visita.js
+++ routes/visita.js
@@ -82,8 +82,8 @@ exports.cods = function(req, res) {
                         jimps.read('C:/Users/Go Jump/Desktop/Jump/public/cods/base.png', function (err,image2) {
                             if(err) console.log(err);
                             jimps.loadFont(jimps.FONT_SANS_32_BLACK).then(function (font) {
-                                image2.print(font, 350, 20, req.params.cod.toString());
-                                image2.print(font, 450, 20, req.session.jumps[i][1].toString());
+                                image2.print(font, 0, 20, req.params.cod.toString());
+                                image2.print(font, 100, 20, req.session.jumps[i][1].toString());
                                 image2.rotate(90);
                                 image2.write( pat, function (){ 
                                     res.redirect('cods/' + req.params.cod.toString() + req.session.jumps[i][1].toString() + '.png');
